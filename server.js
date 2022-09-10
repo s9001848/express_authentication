907df959cdbf91b8ac8540f430a09cc37da2f4a9
s9001848 <yenwu@ucdavis.edu>
2022-09-10 15:48:20 -0700

    server: Import passport and pass through middleware

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const layouts = require('express-ejs-layouts');
 const app = express();
 const session = require('express-session');
 const flash = require('connect-flash');
+const passport = require('./config/ppConfig');
 
 const SECRET_SESSION = process.env.SECRET_SESSION;
 console.log('yooooo.....>>>', SECRET_SESSION);
@@ -22,6 +23,9 @@ app.use(session({
 }));
 
 app.use(flash());            // flash middleware
+
+app.use(passport.initialize());      // Initialize passport
+app.use(passport.session());         // Add a session
 
 app.use((req, res, next) => {
   console.log('res locals >>>', res.locals);
